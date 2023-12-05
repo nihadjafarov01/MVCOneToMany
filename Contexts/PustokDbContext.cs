58a46ed98bd45a0ed50aab98db33d75bbfb61b9a
nihadjafarov01 <mi72kv3d0@code.edu.az>
2023-12-05 05:39:15 +0400

    first commit

---
+++ Contexts/PustokDbContext.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Contexts
+{
+    public class PustokDbContext : DbContext
+    {
+        public PustokDbContext(DbContextOptions opt) : base(opt) { }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ ViewModels/CategoryVM/CategoryListItemVM.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.ViewModels.CategoryVM
+{
+    public class CategoryListItemVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
